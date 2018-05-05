da124c2c7903ab60f45dc22cbcbe6cc3990ca8fa
Francisco <Francisco@192.168.1.9>
2018-05-05 15:15:34 -0300

    Adicionando atributos dentro da class produto

--- src/main/java/br/com/cactusteam/grocery/model/Produto.java
+++ src/main/java/br/com/cactusteam/grocery/model/Produto.java
@@ -1,5 +1,41 @@
 package br.com.cactusteam.grocery.model;
 
 public class Produto {
+	
+	private Long id;
+	private String nome;
+	private String codigoDeBarra;
+	private String marca;
+	
+	public Long getId() {
+		return id;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getCodigoDeBarra() {
+		return codigoDeBarra;
+	}
+	public void setCodigoDeBarra(String codigoDeBarra) {
+		this.codigoDeBarra = codigoDeBarra;
+	}
+	public String getMarca() {
+		return marca;
+	}
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	
+	
+	
+	
+	
+	
 
 }
